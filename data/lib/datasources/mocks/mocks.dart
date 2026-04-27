82946bd1df47d42e83378ff0cdf2de1bb0968cc0
Damir <imyaminovdamir10@gmail.com>
2026-04-27 18:16:39 +0600

    refactor: remove unused profile tags mock datasource and rename auth mock export

--- data/lib/datasources/mocks/mocks.dart
+++ data/lib/datasources/mocks/mocks.dart
@@ -1,6 +1,5 @@
 export 'apartaments_mock_datasource.dart';
-export 'auth_mock.dart';
+export 'auth_mock_data_source.dart';
 export 'location_catalog_mock_datasource.dart';
 export 'mock_storage.dart';
 export 'profile_mock_data_source.dart';
-export 'profile_tags_mock_datasource.dart';
