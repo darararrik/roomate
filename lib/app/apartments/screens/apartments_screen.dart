cf42ebd7d6f1891ee7fd6b1ca92f83ae4afa3998
Damir <imyaminovdamir10@gmail.com>
2026-05-01 19:58:54 +0600

    fix

--- lib/app/apartments/screens/apartments_screen.dart
+++ lib/app/apartments/screens/apartments_screen.dart
@@ -1,8 +1,6 @@
-import 'package:flutter/material.dart';
-
 import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-
 import 'package:roomate/lib.dart';
 import 'package:roomate/routing/app_routing.gr.dart';
 
@@ -22,34 +20,33 @@ class ApartamentsScreen extends ConsumerWidget {
           MainAppBar(
             bottom: ListControlsAppBarBottom(
               optionsCount: asyncState.value?.apartaments.length ?? 0,
-              onFiltersTap: () =>
-                  ref.read(apartamentFilterProvider.notifier).openFilters(),
+              onFiltersTap: () => ref.read(apartamentFilterProvider.notifier).openFilters(),
             ),
           ),
           asyncState.when(
             data: (state) {
               final apartaments = state.apartaments;
-              return SliverList.separated(
-                itemCount: apartaments.length,
-                itemBuilder: (context, index) {
-                  final apartment = apartaments[index];
-                  return ApartmentCard(
-                    apartment: apartment,
-                    isFavorite: favorites.contains(apartment.id),
-                    onFavoriteTap: () => favoritesNotifier.toggle(apartment.id),
-                    onTap: () => context.pushRoute(
-                      ApartamnetRoute(apartment: apartment),
-                    ),
-                  );
-                },
-                separatorBuilder: (context, index) {
-                  return const SizedBox(height: S.p12);
-                },
+              return SliverPadding(
+                padding: const P(bottom: S.p24),
+                sliver: SliverList.separated(
+                  itemCount: apartaments.length,
+                  itemBuilder: (context, index) {
+                    final apartment = apartaments[index];
+                    return ApartmentCard(
+                      apartment: apartment,
+                      isFavorite: favorites.contains(apartment.id),
+                      onFavoriteTap: () => favoritesNotifier.toggle(apartment.id),
+                      onTap: () => context.pushRoute(ApartamnetRoute(apartment: apartment)),
+                    );
+                  },
+                  separatorBuilder: (context, index) {
+                    return const SizedBox(height: S.p12);
+                  },
+                ),
               );
             },
             loading: () => const SliverToBoxAdapter(child: LoadingWidget()),
-            error: (error, stack) =>
-                SliverToBoxAdapter(child: ErrorView(error: stack)),
+            error: (error, stack) => SliverToBoxAdapter(child: ErrorView(error: stack)),
           ),
         ],
       ),
--- lib/widgets/app_bars/list_controls_app_bar_bottom.dart
+++ lib/widgets/app_bars/list_controls_app_bar_bottom.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-
 import 'package:roomate/lib.dart';
 
-class ListControlsAppBarBottom extends StatelessWidget
-    implements PreferredSizeWidget {
+class ListControlsAppBarBottom extends StatelessWidget implements PreferredSizeWidget {
   const ListControlsAppBarBottom({
     super.key,
     required this.optionsCount,
@@ -18,89 +16,70 @@ class ListControlsAppBarBottom extends StatelessWidget
 
   @override
   Widget build(BuildContext context) {
-    return ColoredBox(
-      color: context.colors.graysWhite,
-      child: SafeArea(
-        top: false,
-        bottom: false,
-        child: Column(
-          children: [
-            SizedBox(
-              height: S.p64,
-              child: Padding(
-                padding: const P(top: S.p8, left: S.p16, right: S.p16),
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  padding: const P(vertical: S.p8, horizontal: S.p8),
-                  children: [
-                    FilterCard(
-                      leading: const AppIcon(
-                        AppIcons.filter2,
-                        width: S.p24,
-                        height: S.p24,
-                      ),
-                      title: context.l10n.filters,
-                      onTap: onFiltersTap,
+    return SafeArea(
+      top: false,
+      bottom: false,
+      child: Column(
+        children: [
+          SizedBox(
+            height: S.p64,
+            child: Padding(
+              padding: const P(top: S.p8, left: S.p16, right: S.p16),
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                padding: const P(vertical: S.p8, horizontal: S.p8),
+                children: [
+                  FilterCard(
+                    leading: const AppIcon(AppIcons.filter2, width: S.p24, height: S.p24),
+                    title: context.l10n.filters,
+                    onTap: onFiltersTap,
+                  ),
+                  FilterCard(
+                    trailing: const AppIcon(AppIcons.arrowDown, width: S.p24, height: S.p24),
+                    title: context.l10n.term,
+                    onTap: () => showModalBottomSheet(
+                      context: context,
+                      isScrollControlled: true,
+                      builder: (context) => const RentDurationBottomSheet(),
                     ),
-                    FilterCard(
-                      trailing: const AppIcon(
-                        AppIcons.arrowDown,
-                        width: S.p24,
-                        height: S.p24,
-                      ),
-                      title: context.l10n.term,
-                      onTap: () => showModalBottomSheet(
-                        context: context,
-                        isScrollControlled: true,
-                        builder: (context) => const RentDurationBottomSheet(),
-                      ),
+                  ),
+                  FilterCard(
+                    trailing: const AppIcon(AppIcons.arrowDown, width: S.p24, height: S.p24),
+                    title: context.l10n.district,
+                    onTap: () => showModalBottomSheet(
+                      context: context,
+                      isScrollControlled: true,
+                      builder: (context) => const DistrictBottomSheet(),
                     ),
-                    FilterCard(
-                      trailing: const AppIcon(
-                        AppIcons.arrowDown,
-                        width: S.p24,
-                        height: S.p24,
-                      ),
-                      title: context.l10n.district,
-                      onTap: () => showModalBottomSheet(
-                        context: context,
-                        isScrollControlled: true,
-                        builder: (context) => const DistrictBottomSheet(),
-                      ),
-                    ),
-                  ].separated(const SizedBox(width: S.p12)),
-                ),
+                  ),
+                ].separated(const SizedBox(width: S.p12)),
               ),
             ),
-            SizedBox(
-              height: S.p64,
-              child: Padding(
-                padding: const P(left: S.p24, right: S.p24, bottom: S.p8),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "${context.l10n.optionsFound} $optionsCount",
-                      style: context.typography.bodyTitle,
+          ),
+          SizedBox(
+            height: S.p64,
+            child: Padding(
+              padding: const P(left: S.p24, right: S.p24, bottom: S.p8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "${context.l10n.optionsFound} $optionsCount",
+                    style: context.typography.bodyTitle,
+                  ),
+                  GestureDetector(
+                    onTap: () => showModalBottomSheet(
+                      context: context,
+                      isScrollControlled: true,
+                      builder: (context) => SortBottomSheet(),
                     ),
-                    GestureDetector(
-                      onTap: () => showModalBottomSheet(
-                        context: context,
-                        isScrollControlled: true,
-                        builder: (context) => SortBottomSheet(),
-                      ),
-                      child: const AppIcon(
-                        AppIcons.sort,
-                        width: S.p32,
-                        height: S.p32,
-                      ),
-                    ),
-                  ],
-                ),
+                    child: const AppIcon(AppIcons.sort, width: S.p32, height: S.p32),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
